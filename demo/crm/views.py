0132e17af2759c955cb320fe9f21b3094d8e1893
Christian <kalonjic34@gmail.com>
2025-08-01 17:36:49 +0200

    Implement homepage and register views

--- demo/crm/views.py
+++ demo/crm/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse('This is the home page')
+
+def register(request):
+    return HttpResponse('This is the registration page')
