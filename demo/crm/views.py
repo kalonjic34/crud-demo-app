a4255df05bcddcf6736b85343b981ae9c2492510
Christian <kalonjic34@gmail.com>
2025-08-02 16:17:57 +0200

    Implement task view with single task display

--- demo/crm/views.py
+++ demo/crm/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Task
+
 def homepage(request):
     return render(request, 'crm/index.html')
+
+def task(request):
+    
+    queryDataSingle = Task.objects.get(id=3)
+    context = {'singleTask': queryDataSingle}
+    
+    return render(request, 'crm/task.html',context)
 
 def register(request):
     return render(request,'crm/register.html')
