95a85ecefa39d1fae14f6349d94e85988a69b788
Christian <kalonjic34@gmail.com>
2025-08-04 15:32:08 +0200

    Change task view to display all tasks

--- demo/crm/views.py
+++ demo/crm/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
@@ -9,19 +9,27 @@ from .forms import TaskForm
 def homepage(request):
     return render(request, 'crm/index.html')
 
-def task(request):
+def tasks(request):
     
-    queryDataSingle = Task.objects.get(id=3)
-    context = {'singleTask': queryDataSingle}
+    queryDataAll = Task.objects.all()
+    context = {'AllTask': queryDataAll}
     
-    return render(request, 'crm/task.html',context)
+    return render(request, 'crm/view-tasks.html',context)
 
 def register(request):
     return render(request,'crm/register.html')
 
-def task_form(request):
+def create_task(request):
     form = TaskForm()
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('view-tasks')
      
     context = {'TaskForm':form}
     
-    return render(request, 'crm/task-form.html',context)
+    return render(request, 'crm/create-task.html',context)
