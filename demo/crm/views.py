2be826fc3873b45b90c2d8cc1f71922de8505757
Christian <kalonjic34@gmail.com>
2025-08-04 16:13:01 +0200

    Add update task view and functionality

--- demo/crm/views.py
+++ demo/crm/views.py
@@ -9,6 +9,8 @@ from .forms import TaskForm
 def homepage(request):
     return render(request, 'crm/index.html')
 
+# read task
+
 def tasks(request):
     
     queryDataAll = Task.objects.all()
@@ -16,8 +18,12 @@ def tasks(request):
     
     return render(request, 'crm/view-tasks.html',context)
 
+# registration webpage
+
 def register(request):
     return render(request,'crm/register.html')
+
+# create a task
 
 def create_task(request):
     form = TaskForm()
@@ -33,3 +39,21 @@ def create_task(request):
     context = {'TaskForm':form}
     
     return render(request, 'crm/create-task.html',context)
+
+
+def update_task(request, pk):
+   task = Task.objects.get(id=pk)
+   
+   form = TaskForm(instance=task)
+   
+   if request.method == 'POST':
+       form = TaskForm(request.POST, instance=task)
+       
+       if form.is_valid():
+           form.save()
+           
+           return redirect('view-tasks')
+       
+       context = {'UpdateTask':form}
+       
+       return render(request, 'crm/update-task.html',context)
