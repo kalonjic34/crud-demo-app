4bdc40dcdee78943230489faef6dcea2670fff32
Christian <kalonjic34@gmail.com>
2025-08-04 16:13:20 +0200

    Add update-task URL for task management

--- demo/crm/urls.py
+++ demo/crm/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.homepage, name = ""),
     path('view-tasks',views.tasks, name = "view-tasks"),
     path('register',views.register , name="register"),
-    path('create-task',views.create_task, name="create-task")
+    path('create-task',views.create_task, name="create-task"),
+    path('update-task/<str:pk>',views.update_task, name="update-task")
 ]
