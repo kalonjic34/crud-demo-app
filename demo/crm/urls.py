9d631db6c6fcf368a8e17fb4e079a1fc0b80e2e2
Christian <kalonjic34@gmail.com>
2025-08-04 15:31:16 +0200

    Rename 'task' to 'view-tasks' and 'task-form' to 'create-task'

--- demo/crm/urls.py
+++ demo/crm/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage, name = ""),
-    path('task',views.task, name = "task"),
+    path('view-tasks',views.tasks, name = "view-tasks"),
     path('register',views.register , name="register"),
-    path('task-form',views.task_form, name="task-form")
+    path('create-task',views.create_task, name="create-task")
 ]
