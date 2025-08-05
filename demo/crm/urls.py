d53503e197b7ac2b45e1f2896f19a6b8e2477cd0
Christian <kalonjic34@gmail.com>
2025-08-05 14:20:04 +0200

    Add my-login and dashboard paths to URL configuration

--- demo/crm/urls.py
+++ demo/crm/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('register',views.register , name="register"),
     path('create-task',views.create_task, name="create-task"),
     path('update-task/<str:pk>',views.update_task, name="update-task"),
-    path('delete-task/<str:pk>',views.delete_task, name="delete-task")
+    path('delete-task/<str:pk>',views.delete_task, name="delete-task"),
+    path('my-login',views.my_login, name ='my-login'),
+    path('dashboard',views.dashboard, name ='dashboard')
 ]
