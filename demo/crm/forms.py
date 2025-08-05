b985321c6f11e8aa321cc9e078d5b6eb9c61f4c2
Christian <kalonjic34@gmail.com>
2025-08-05 14:19:45 +0200

    Refactor forms.py to include user authentication forms

--- demo/crm/forms.py
+++ demo/crm/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Task
 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = '__all__'  
+        fields = '__all__' 
+        
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2'] 
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
